f362c6c93e8482006d7f1d25dbaca71804009c67
Parokq <alekseyherman@gmail.com>
2024-08-01 23:05:56 +0300

    drafts for ftp screen component

--- shared/src/commonMain/kotlin/com/justparokq/homeftp/shared/main/MainComponent.kt
+++ shared/src/commonMain/kotlin/com/justparokq/homeftp/shared/main/MainComponent.kt
@@ -1,6 +1,52 @@
 package com.justparokq.homeftp.shared.main
 
+
+// todo remove from compose-ui module
+sealed interface FileSystemObject {
+
+    data class Directory(
+        val name: String,
+        val content: List<FileSystemObject>?,
+    ) : FileSystemObject
+
+    data class File(
+        val name: String,
+        val content: Any, // todo update
+    ) : FileSystemObject
+}
+
+
+// todo rename feature screen
+data class FtpClientScreenModel(
+    val currentPath: String,
+    val fileTree: FileSystemObject.Directory,
+    val isLoading: Boolean,
+    val showHierarchyView: Boolean,
+)
+
 interface MainComponent {
 
-    fun onShowWelcomeClicked()
+    /**
+     * Navigates to directory by path
+     */
+    fun onDirectoryClicked(dirPath: List<String>)
+
+    /**
+     * Open file
+     * If file is Directory - navigates to it
+     * If file is File - opens it (photo or video or smt else)
+     * */
+    fun onFileClicked(file: FileSystemObject.File)
+
+    /**
+     * Closes hierarchyView if it was open and otherwise
+     * */
+    fun onToggleHierarchyView()
+
+    /**
+     * Not for MVP
+     * Apply sorting to all files in MainView (currently opened directory)
+     * This setting should persist between directories?
+     * */
+    fun onSortingApplyClicked() {}
 }
